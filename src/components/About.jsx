1083107e68ab117424b4e0c5049629bd9bc5196c
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-08-11 21:58:02 +0530

    protfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Meet Ankur Sasmal “I am a self-starter with strong interpersonal
+          skills. I work efficiently both as an individual contributor as well
+          as along with a team. I seek new challenges and try to think
+          out-of-the-box while looking for creative solutions to a given
+          problem.”
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          MAULANA ABUL KALAM AZAD UNIVERSITY OF TECHNOLOGY, WEST
+          BENGAL(W.B.U.T)(2022- 2026 )
+          <br />
+          B.Tech Information Technology
+          <br /> Courses:Data Structure(DSA), OOPS
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          <b>Programming Languages:</b>
+          <br />
+          C and C++ (Proficient),Proficient on Object-Oriented Programming,
+          Python HTML,CSS, Boostrap,Tailwind CSS,SCSS,SASSS JavaScript , Mysql
+          ,React Js,TypeScript,Node js,Express JS,MongoDB,. REST API,Next js.
+          <br />
+          <b>Data structures and algorithms:</b>
+          <br />
+          Using c++(intermediate).
+          <br />
+          <b>Libraries / Frameworks::</b>
+          <br />
+          working experience in React JS.
+          <br />
+          <b>Version Control Tools and IDE:</b>
+          <br />
+          Git, Github, Visual Studio Code, python idle,MySqlWorkbench,Pycharm
+          <br />
+          <b>Soft Skills:</b>
+          <br />
+          Collabora on, Communication, Planning, Teamwork, Time Management,
+          Leadership
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          <b> 1.Freelance Developer</b>
+          <br />
+          <b> Self-employed</b>
+          <br /> Duration (January 2023 - Present) <br />
+          Responsibilities and Achievements: <br /> Developed and delivered
+          customized web solutions for various clients, ensuring responsiveness
+          and user-friendly design. <br /> Worked on multiple projects
+          simultaneously, meeting deadlines and exceeding client expectations.{" "}
+          <br /> Utilized technologies such as HTML, CSS, JavaScript, React, and
+          Node.js to create dynamic web applications. <br />
+          <br />
+          <b>2.Greencode Software</b> <b>(internship)</b>
+          <br /> <b>Position: </b>
+          <br /> <b>Software Developer intern</b> <br />
+          Duration:- (1st Aug 2024 - present ) <br />
+          Description and Achievements:- <br /> . Research and learn about
+          various programming topics
+          <br /> . Collaborating on building small projects from scratch to
+          production ready
+          <br /> . Be involved in the company product, from coding to outreach
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <b>• Achievement 1:</b>
+        <br />
+        certificate on college hackathon
+        <br />
+        <b>• Leetcode:</b>
+        <br />
+        80+ Problem solved
+        <br />
+        <b>• HackerRank: </b>
+        <br />
+        4 star
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          "As a passionate B.Tech IT student with a dedication to continuous
+          learning and problem-solving, I aim to leverage my skills in software
+          development to create innovative solutions that improve lives and
+          contribute to technological advancement. My mission is to explore the
+          ever-evolving world of technology, while striving to develop
+          applications that are user-centric, accessible, and impactful."
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
